4c7c9bd1bf3086ba3180db8fb3e59566de0b284c
Farshad Nematdoust <farshad@nematdoust.com>
2020-02-16 20:05:18 +0100

    Add PostgresSQl module

---
+++ PostgreSQLChecker/postgresqlChecker.go
@@ -0,0 +1,52 @@
+package PostgreSQLChecker
+
+import (
+	"database/sql"
+	"errors"
+	_ "github.com/lib/pq"
+	"wait4it/model"
+)
+
+func (pq *PostgresSQLConnection) BuildContext(cx model.CheckContext) {
+	pq.Port = cx.Port
+	pq.Host = cx.Host
+	pq.Username = cx.Username
+	pq.Password = cx.Password
+	pq.DatabaseName = cx.DatabaseName
+	if len(pq.SSLMode) < 1 {
+		pq.SSLMode = "disable"
+	} else {
+		pq.SSLMode = cx.DBConf.SSLMode
+	}
+}
+
+func (pq *PostgresSQLConnection) Validate() (bool, error) {
+	if len(pq.Host) == 0 || len(pq.Username) == 0 {
+		return false, errors.New("host or username can't be empty")
+	}
+
+	if pq.Port < 0 || pq.Port > 65535 {
+		return false, errors.New("invalid port range for mysql")
+	}
+
+	return true, nil
+}
+
+func (pq *PostgresSQLConnection) Check() (bool, bool, error) {
+	dsl := pq.BuildConnectionString()
+
+	db, err := sql.Open("postgres", dsl)
+
+	// if there is an error opening the connection, handle it
+	if err != nil {
+		return false, true, err
+	}
+
+	err = db.Ping()
+	if err != nil {
+		return false, false, nil
+	}
+	_ = db.Close()
+
+	return true, true, nil
+}
