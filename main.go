d8c0867693bbce50081fc88ddbb28259ae835f98
Farshad Nematdoust <farshad@nematdoust.com>
2020-02-26 22:10:52 +0100

    Update main func to use new input parsers

--- main.go
+++ main.go
@@ -2,10 +2,14 @@ package main
 
 import (
 	"wait4it/cmd"
-	"wait4it/inputParser"
+	"wait4it/inputParser/envParser"
+	"wait4it/inputParser/flagParser"
+	"wait4it/model"
 )
 
 func main() {
-	c := inputParser.GetInput()
-	cmd.RunCheck(c)
+	c := &model.CheckContext{}
+	c = envParser.Parse(c)
+	c = flagParser.Parse(c)
+	cmd.RunCheck(*c)
 }
