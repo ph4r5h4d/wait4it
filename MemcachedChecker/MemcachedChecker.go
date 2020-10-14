a2a9a49eadb12aea469d06959d11dac8d80428b0
MatinRh <itsmatinrh@gmail.com>
2020-10-15 01:03:36 +0330

    change checking conditions for correctness of port and Memcached instance/instances connection

--- MemcachedChecker/MemcachedChecker.go
+++ MemcachedChecker/MemcachedChecker.go
@@ -17,7 +17,7 @@ func (m *MemcachedConnection) Validate() (bool, error) {
 		return false, errors.New("Host can't be empty")
 	}
 
-	if m.Port < 0 || m.Port > 65535 {
+	if m.Port < 1 {
 		return false, errors.New("Invalid port range for Memcached")
 	}
 
@@ -27,17 +27,9 @@ func (m *MemcachedConnection) Validate() (bool, error) {
 func (m *MemcachedConnection) Check() (bool, bool, error) {
 	mc := memcache.New(m.BuildConnectionString())
 
-	// Since the current version of the Memcached Golang client doesn't create
-	// any connection to the Memcached server (or at least the client that I'm using)
-	// at the New method and at here we don't have any way for checking the correctness
-	// of the given server address, we must use another way for checking connection. As I
-	// understand, it seems the connection will establish at setting and getting items time,
-	// and I think this is a good point for checking connection.
-	err := mc.Set(&memcache.Item{Key: "foo", Value: []byte("bar")})
-	if err != nil {
-		return false, true, err
+	if err := mc.Ping(); err != nil {
+		return false, false, err
 	}
-	mc.Delete("foo")
 
 	return true, true, nil
 }
