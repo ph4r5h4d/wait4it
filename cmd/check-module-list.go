2925cff2c7f17011d1f92f1ae279299dd8ae0c62
Matin Rahmanian <itsmatinrh@gmail.com>
2020-10-18 07:34:21 -0700

    add ElasticSearch support

--- cmd/check-module-list.go
+++ cmd/check-module-list.go
@@ -7,17 +7,19 @@ import (
 	"wait4it/PostgreSQLChecker"
 	"wait4it/RedisChecker"
 	"wait4it/TcpChecker"
+	"wait4it/elasticsearch"
 	"wait4it/httpChecker"
 	"wait4it/rabbitmq"
 )
 
 var cm = map[string]interface{}{
-	"tcp":      &TcpChecker.Tcp{},
-	"mysql":    &MySQLChecker.MySQLConnection{},
-	"postgres": &PostgreSQLChecker.PostgresSQLConnection{},
-	"http":     &httpChecker.HttpCheck{},
-	"mongo":    &MongoDbChecker.MongoDbConnection{},
-	"redis":    &RedisChecker.RedisConnection{},
-	"rabbitmq": &rabbitmq.RabbitChecker{},
-  "memcached": &MemcachedChecker.MemcachedConnection{},
+	"tcp":           &TcpChecker.Tcp{},
+	"mysql":         &MySQLChecker.MySQLConnection{},
+	"postgres":      &PostgreSQLChecker.PostgresSQLConnection{},
+	"http":          &httpChecker.HttpCheck{},
+	"mongo":         &MongoDbChecker.MongoDbConnection{},
+	"redis":         &RedisChecker.RedisConnection{},
+	"rabbitmq":      &rabbitmq.RabbitChecker{},
+	"memcached":     &MemcachedChecker.MemcachedConnection{},
+	"elasticsearch": &elasticsearch.ElasticSearchChecker{},
 }
--- go.mod
+++ go.mod
@@ -4,6 +4,7 @@ go 1.15
 
 require (
 	github.com/bradfitz/gomemcache v0.0.0-20190913173617-a41fca850d0b
+	github.com/elastic/go-elasticsearch/v8 v8.0.0-20201007143536-4b4020669208
 	github.com/go-redis/redis/v8 v8.0.0-beta.8
 	github.com/go-sql-driver/mysql v1.5.0
 	github.com/lib/pq v1.3.0
